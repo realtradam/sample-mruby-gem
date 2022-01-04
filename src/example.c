c69df83e22065b029d85f4c7f6ce72be9fbfc75f
realtradam <github@tradam.dev>
2022-01-04 13:38:48 -0500

    .

--- src/example.c
+++ src/example.c
@@ -2,8 +2,8 @@
 #include <mruby/string.h>
 #include <stdio.h>
 
-static mrb_value
-mrb_pog(mrb_state *mrb, mrb_value self)
+	static mrb_value
+mrb_c_method(mrb_state *mrb, mrb_value self)
 {
 	mrb_ensure_string_type(mrb, self);
 	printf("%s: A C Extension\n", mrb_str_to_cstr(mrb, self));
@@ -12,12 +12,12 @@ mrb_pog(mrb_state *mrb, mrb_value self)
 }
 
 void
-mrb_my_silly_extension_gem_init(mrb_state* mrb) {
-	struct RClass *class_emote = mrb_define_module(mrb, "Emote");
-	mrb_define_class_method(mrb, class_emote, "pog", mrb_pog, MRB_ARGS_NONE());
+mrb_c_and_ruby_extension_example_gem_init(mrb_state* mrb) {
+	struct RClass *class_cextension = mrb_define_module(mrb, "CRubyExtension");
+	mrb_define_class_method(mrb, class_cextension, "c_method", mrb_c_method, MRB_ARGS_NONE());
 }
 
 void
-mrb_my_silly_extension_gem_final(mrb_state* mrb) {
+mrb_c_and_ruby_extension_example_gem_final(mrb_state* mrb) {
 	/* finalizer */
 }
